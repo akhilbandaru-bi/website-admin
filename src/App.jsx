0ba676bd64ec2d5421b21ce2242f148e7fab7dba
sandeep-6 <sandeep.saliganti@x-verity.com>
2025-11-08 11:51:25 +0530

    fixed the folder issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
-import Login from './pages/Login';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Login1 from '../src/pages/Login1';
+import Dashboard1 from '../src/pages/Dashboard1';
+import Pages1 from '../src/pages/Pages1';
+import Services1 from '../src/pages/Services1';
+import Solutions1 from '../src/pages/Solutions1';
+import Verticals1 from '../src/pages/Verticals1';
+import Blogs1 from '../src/pages/Blog';
+import CaseStudies1 from '../src/pages/CaseStudies1';  
+import Leads1 from '../src/pages/Leads1';
+import SubscribedUsers1 from '../src/pages/SubscribedUsers1';
+import DashboardLayout from '../src/layouts/DashboardLayout';
 
 function App() {
-  return <Login />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login1 />} />
+        <Route path="/dashboard" element={<DashboardLayout />}>
+          <Route index element={<Dashboard1 />} />
+          <Route path="pages" element={<Pages1 />} />
+          <Route path="services" element={<Services1 />} />
+          <Route path="solutions" element={<Solutions1 />} />
+          <Route path="verticals" element={<Verticals1 />} />
+          <Route path="blogs" element={<Blogs1 />} />
+          <Route path="case-studies" element={<CaseStudies1 />} />
+          <Route path="leads" element={<Leads1 />} />
+          <Route path="subscribed-users" element={<SubscribedUsers1 />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/SubscribedUsers1.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Squircle } from '@squircle-js/react';
+import './PageTemplate1.css';
+
+const SubscribedUsers = () => {
+  return (
+    <div className="page-template">
+      <div className="page-header">
+        <h1 className="page-title">Subscribed Users</h1>
+        <p className="page-subtitle">Manage your subscribed users and newsletter</p>
+      </div>
+
+      <Squircle
+        cornerRadius={24}
+        cornerSmoothing={1}
+        className="content-card"
+      >
+        <div className="card-content">
+          <p className="placeholder-text">Subscribed users management interface will appear here</p>
+        </div>
+      </Squircle>
+    </div>
+  );
+};
+
+export default SubscribedUsers;
+
